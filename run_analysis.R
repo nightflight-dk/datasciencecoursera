d9d5c5c7d88e6cf3aef02b16d4761aa9a6cb869c
Damian K <nightflight.dk@gmail.com>
2015-01-26 00:09:34 +0100

    Getting and cleaning data - prog. assignment

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+run_analysis <- function(){
+  test<-read.table("test\\X_test.txt")
+  train<-read.table("train\\X_train.txt")
+  
+  ft<-read.table("features.txt",sep=" ")
+  ffilter<-grepl("-std()",ft$V2,fixed=TRUE) | grepl("-mean()",ft$V2,fixed=TRUE)
+  
+  #1/2 Merges the training and the test sets to create one data set.
+  clean<-rbind(test[,ffilter],train[,ffilter])
+  remove(test)
+  remove(train)
+  
+  #3/4 Appropriately labels the data set with descriptive variable names. 
+  colNames<-as.character(ft[ffilter,2])
+  clean<-setNames(clean,colNames)  
+  
+  #5
+  means<-colMeans(clean)
+  
+  return(means)
+}
